0fd8439c6f655951f7680154951d57739182c2f2
MarcoRBosco <marco.bosco@uamp.io>
2024-11-19 23:48:52 +0100

    learning

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,22 +1,20 @@
+use bt_hci::cmd::info;
 use defmt::*;
 use modbus_core::rtu::{server, RequestAdu};
 use {defmt_rtt as _, panic_probe as _};
 
-pub fn decode_serial_buffer() { //Just for testing prupose
-    // Example buffer received from the master
-    let received_buffer: &[u8] = &[0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xC4, 0x0B];
-
-    // Decode the received buffer
-    match server::decode_request(received_buffer) {
+pub fn decode_serial_buffer(buf: &[u8]) {
+    match server::decode_request(buf) {
         Ok(Some(request)) => {
             info!("ID slave target: {:?}", request.hdr.slave);
+            //info!("Function code: {:?}", request.pdu);
             // Process the request here
         }
         Ok(None) => {
             info!("No request decoded");
         }
         Err(e) => {
-            ;//info!("Error decoding request: {:?}", e);
+            info!("Error decoding");//info!("Error decoding request: {:?}", e);
         }
     }
 
